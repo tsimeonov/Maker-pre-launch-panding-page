c927148e16747ebdce6637c7926f9ea643f35334
tsimeonov <tsimeonov12@gmail.com>
2022-11-29 14:04:12 +0100

    done with footer

--- code/app.js
+++ code/app.js
@@ -1,1 +1,18 @@
+"use strict";
 
+let inputForm = document.querySelector("#input-form");
+let errorMsg = document.querySelector("#error");
+let btn = document.querySelector("#btn");
+let emailRegex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (emailRegex.test(inputForm.value)) {
+    inputForm.style.border = "none";
+    errorMsg.style.display = "none";
+  } else {
+    inputForm.style.border = "1px solid red";
+    errorMsg.style.display = "block";
+  }
+});
